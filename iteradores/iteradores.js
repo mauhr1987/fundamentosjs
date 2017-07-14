4736f45db38049950a8c88cd1baf7a5be4b86fae
Mauhr1987 <mauricio.hidalgo@dotcreek.com>
2017-07-13 23:08:33 -0600

    Iteradores

---
+++ iteradores/iteradores.js
@@ -0,0 +1,37 @@
+//FORMA 1
+function fibonacci(){
+	let a = 0, b = 1;
+	return {
+		next: function(){
+			let f = a;
+			a = b;
+			b = f + a	
+			return { value: f, done: false }
+		}
+	};
+}
+const fibo = fibonacci()
+fibo.next().value
+
+
+//FORMA 2
+function fibonacci(){
+	let a = 0, b = 1;
+	return {
+		next: function(){
+			let f = a;
+			a = b;
+			b = f + a	
+			return { value: f, done: false }
+		}
+	};
+}
+const fibo = {};
+fibo[Symbol.iterator] = fibonacci;
+
+let i=0;
+for (let value of fibo) {
+	console.log(value);
+	i++;
+	if(i>20) break;
+}
